3f321aef3fa12c3d5c76abe316e24ae0670f090e
karinapatel <karinapatel@sbcglobal.net>
2019-03-25 17:59:55 -0700

    day's work

--- model1.py
+++ model1.py
@@ -3,21 +3,20 @@ from sklearn.model_selection import train_test_split,cross_val_score
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import roc_auc_score, f1_score, accuracy_score
+
 #modeling:
 def my_model(model_name):
-    df_train,y_train,df_test,y_test= prep_data()
-
     model = model_name
     model.fit(df_train,y_train)
     
-    print("TRAIN on {}".format(model_name))
+    print("TRAIN on {}".format(model_name[:-2]))
     print("ROC_AUC score: ", cross_val_score(LogisticRegression(),df_train,y_train,cv=5,scoring='roc_auc').mean())
     print("Accuracy score: ", cross_val_score(LogisticRegression(),df_train,y_train,cv=5).mean())
     print("f1 score: ", cross_val_score(LogisticRegression(),df_train,y_train,cv=5,scoring='f1').mean())
 
     y_pred = model.predict(df_test)
 
-    print("TEST on {}".format(model_name))
+    print("TEST on {}".format(model_name[:-2]))
     print("ROC_AUC score: ", roc_auc_score(y_test,y_pred))
     print("Accuracy score: ", accuracy_score(y_test,y_pred))
     print("f1 score: ", f1_score(y_test,y_pred))
@@ -57,4 +56,11 @@ def my_model(model_name):
     # print("f1 RF: ", f1_score(y_test,y_pred))
 
 if __name__ == "__main__":
+    df_train,y_train,df_test,y_test= prep_data()
+    print("Logistic: ")
     my_model(LogisticRegression())
+
+    print("RandomForest")
+    my_model(RandomForest())
+
+
